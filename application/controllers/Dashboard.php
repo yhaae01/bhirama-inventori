12f0f1e99ddf4a3e36533c34f48ee40d5f9f99db
yhaae01 <suryaintpermana@gmail.com>
2022-05-16 08:40:12 +0700

    Merubah title dashboard

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -5,10 +5,15 @@ class Dashboard extends CI_Controller
 {
 	public function index()
 	{
-		$this->load->view('templates/header');
-		$this->load->view('templates/topbar');
-		$this->load->view('templates/sidebar');
-		$this->load->view('dashboard');
+		$data['title'] = 'Dashboard';
+        // $data['user'] = $this->db->get_where('user', [
+        //     'username' => $this->session->userdata('username')
+        // ])->row_array();
+
+		$this->load->view('templates/header', $data);
+		$this->load->view('templates/topbar', $data);
+		$this->load->view('templates/sidebar', $data);
+		$this->load->view('dashboard', $data);
 		$this->load->view('templates/footer');
 	}
 }
