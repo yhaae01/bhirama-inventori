a9886b720057f62e7087d91437f5742ec38562a2
ahmadmaulana10 <ahmadmaulana.am96@gmail.com>
2022-05-24 14:48:05 +0700

    Merubah tampilan tambah pesanan

--- application/views/transaksi/pesanan/tambah.php
+++ application/views/transaksi/pesanan/tambah.php
@@ -4,90 +4,51 @@
         <div class="section-header">
             <h1>Pesanan</h1>
         </div>
-
         <div class="section-body">
             <div class="row">
                 <!-- Pesanan -->
-                <div class="col-lg-12 col-md-12 col-sm-12">
+                <div class="col-lg-6 col-md-12 col-sm-12">
                     <div class="card">
                         <div class="card-header">
                             <h4>Tambah Pesanan</h4>
                         </div>
                         <div class="card-body">
                             <form action="" method="post">
+                                <div class="form-group">
+                                    <label>Nama Produk</label>
+                                    <select class="form-control">
+                                        <option>Sirwal Tactical</option>
+                                    </select>
+                                </div>
                                 <div class="row">
-                                    <div class="col-lg-4">
+                                    <div class="col-lg-6">
                                         <div class="form-group">
-                                            <label>Nama Produk</label>
+                                            <label>Ukuran</label>
                                             <select class="form-control">
-                                                <option>Option 1</option>
+                                                <option>XL</option>
                                             </select>
                                         </div>
                                     </div>
-                                    <div class="col-lg-2">
+                                    <div class="col-lg-6">
+                                        <div class="form-group">
+                                            <label>Warna</label>
+                                            <select class="form-control">
+                                                <option>Merah</option>
+                                            </select>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-lg-6">
                                         <div class="form-group">
                                             <label>QTY</label>
                                             <input type="number" class="form-control">
                                         </div>
                                     </div>
-                                    <div class="col-lg-2">
+                                    <div class="col-lg-6">
                                         <div class="form-group">
                                             <label>Harga</label>
-                                            <input type="number" class="form-control" readonly>
-                                        </div>
-                                    </div>
-                                    <div class="col-lg-4">
-                                        <div class="form-group">
-                                            <label>Tanggal</label>
-                                            <input type="date" class="form-control">
-                                        </div>
-                                    </div>
-                                </div>
-
-                                <div class="row">
-                                    <div class="col-lg-4">
-                                        <div class="form-group">
-                                            <label>Pengirim</label>
-                                            <input type="text" class="form-control">
-                                        </div>
-                                    </div>
-                                    <div class="col-lg-4">
-                                        <div class="form-group">
-                                            <label>Penerima</label>
-                                            <input type="text" class="form-control">
-                                        </div>
-                                    </div>
-                                    <div class="col-lg-4">
-                                        <div class="form-group">
-                                            <label>Alamat</label>
-                                            <textarea class="form-control" required=""></textarea>
-                                        </div>
-                                    </div>
-                                </div>
-
-                                <div class="row">
-                                    <div class="col-lg-4">
-                                        <div class="form-group">
-                                            <label>Pengiriman</label>
-                                            <input type="text" class="form-control">
-                                        </div>
-                                    </div>
-                                    <div class="col-lg-4">
-                                        <div class="form-group">
-                                            <label>Kurir</label>
-                                            <select class="form-control">
-                                                <option>JNE Express</option>
-                                                <option>J&T Express</option>
-                                                <option>Sicepat</option>
-                                                <option>Tiki</option>
-                                                <option>Ninja Ekspress</option>
-                                            </select>
-                                        </div>
-                                    </div>
-                                    <div class="col-lg-4">
-                                        <div class="form-group">
-                                            <label>Keterangan</label>
-                                            <textarea class="form-control" required=""></textarea>
+                                            <input type="number" class="form-control">
                                         </div>
                                     </div>
                                 </div>
@@ -99,9 +60,8 @@
                         </form>
                     </div>
                 </div>
-
                 <!-- Detail -->
-                <div class="col-lg-12col-md-12 col-sm-12">
+                <div class="col-lg-6 col-md-12 col-sm-12">
                     <div class="card">
                         <div class="card-header">
                             <h4>Detail Pesanan</h4>
@@ -114,18 +74,22 @@
                                             <th>Nama Produk</th>
                                             <th>QTY</th>
                                             <th>Harga</th>
+                                            <th>Aksi</th>
                                         </tr>
                                     </thead>
                                     <tbody>
                                         <tr>
                                             <td>
-                                                Bhirama Sirwal
+                                                Bhirama Sirwal / XL / Merah
                                             </td>
                                             <td>
                                                 4
                                             </td>
                                             <td>
                                                 200000
+                                            </td>
+                                            <td>
+                                                <a href="" class="btn btn-danger"><i class="fas fa-trash"></i></a>
                                             </td>
                                         </tr>
                                         <tr>
@@ -136,10 +100,6 @@
                                     </tbody>
                                 </table>
                             </div>
-                        </div>
-                        <div class="card-footer text-right">
-                            <button class="btn btn-primary mr-1" type="submit"><i class="fas fa-save"></i> Simpan</button>
-                            <button class="btn btn-secondary" type="reset"><i class="fas fa-undo"></i> Reset</button>
                         </div>
                     </div>
                 </div>
