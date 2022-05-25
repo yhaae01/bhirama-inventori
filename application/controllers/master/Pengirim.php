0d663d001a6ef9ffff6e026d44e542a0b04502b2
Ekawardana <082312931650as@gmail.com>
2022-05-25 10:53:54 +0700

    Menambahkan CRUD pengirim

--- application/controllers/master/Pengirim.php
+++ application/controllers/master/Pengirim.php
@@ -28,4 +28,59 @@ class Pengirim extends CI_Controller
         $this->load->view('master/pengirim/index', $data);
         $this->load->view('templates/footer');
     }
+
+    public function tambah()
+    {
+        $data['title'] = 'Tambah Pengirim';
+        $data['user'] = $this->pengguna->cekPengguna();
+        $data['pengirim'] = $this->pengirim->getAllPengirim();
+        // $data['user'] = $this->db->get_where('user', [
+        //     'username' => $this->session->userdata('username')
+        // ])->row_array();
+
+        $this->form_validation->set_rules('nama_pengirim', 'Pengirim', 'required|trim|is_unique[pengirim.nama_pengirim]', [
+            'required'  => 'Pengirim harus diisi!',
+            'is_unique' => 'Pengirim sudah ada!'
+        ]);
+
+        if ($this->form_validation->run() == FALSE) {
+            $this->load->view('templates/header', $data);
+            $this->load->view('templates/topbar', $data);
+            $this->load->view('templates/sidebar', $data);
+            $this->load->view('master/pengirim/index');
+            $this->load->view('templates/footer');
+        } else {
+            $this->pengirim->tambah_pengirim();
+        }
+    }
+
+    public function ubah()
+    {
+        $data['title'] = 'Ubah Pengirim';
+        $data['user'] = $this->pengguna->cekPengguna();
+        $data['pengirim'] = $this->pengirim->getAllPengirim();
+        // $data['user'] = $this->db->get_where('user', [
+        //     'username' => $this->session->userdata('username')
+        // ])->row_array();
+
+        $this->form_validation->set_rules('nama_pengirim', 'Pengirim', 'required|trim|is_unique[pengirim.nama_pengirim]', [
+            'required'   => 'Pengirim harus diisi!',
+            'is_unique' => 'Pengirim Sudah Ada'
+        ]);
+
+        if ($this->form_validation->run() == FALSE) {
+            $this->load->view('templates/header', $data);
+            $this->load->view('templates/topbar', $data);
+            $this->load->view('templates/sidebar', $data);
+            $this->load->view('master/pengirim/index');
+            $this->load->view('templates/footer');
+        } else {
+            $this->pengirim->ubah_pengirim();
+        }
+    }
+
+    public function hapus($id_pengirim)
+    {
+        $this->pengirim->hapus_pengirim($id_pengirim);
+    }
 }
--- application/models/Pengirim_model.php
+++ application/models/Pengirim_model.php
@@ -8,4 +8,46 @@ class Pengirim_model extends CI_Model
     {
         return $this->db->get('pengirim')->result_array();
     }
+
+    public function tambah_pengirim()
+    {
+        $data = [
+            'nama_pengirim' => htmlspecialchars($this->input->post('nama_pengirim', true)),
+        ];
+
+        $this->db->insert('pengirim', $data);
+
+        $this->session->set_flashdata(
+            'message',
+            'ditambah.'
+        );
+        redirect('master/Pengirim');
+    }
+
+    public function ubah_pengirim()
+    {
+        $id_pengirim = $this->input->post('id_pengirim', true);
+        $data = [
+            'nama_pengirim'          => $this->input->post('nama_pengirim', true),
+        ];
+
+        $this->db->where('id_pengirim', $id_pengirim);
+        $this->db->update('pengirim', $data);
+
+        $this->session->set_flashdata(
+            'message',
+            'diubah.'
+        );
+        redirect('master/Pengirim');
+    }
+
+    public function hapus_pengirim($id_pengirim)
+    {
+        $this->db->delete('pengirim', ['id_pengirim' => $id_pengirim]);
+        $this->session->set_flashdata(
+            'message',
+            'dihapus.'
+        );
+        redirect('master/Pengirim');
+    }
 }
