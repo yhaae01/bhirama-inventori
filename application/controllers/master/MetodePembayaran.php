fa8cc528b9a8a5c9d2a98567579b6ea2741144bc
Ekawardana <082312931650as@gmail.com>
2022-05-25 11:09:22 +0700

    Menambah CRUD Metode Pembayaran

--- application/controllers/master/MetodePembayaran.php
+++ application/controllers/master/MetodePembayaran.php
@@ -29,4 +29,59 @@ class MetodePembayaran extends CI_Controller
         $this->load->view('master/metode-pembayaran/index', $data);
         $this->load->view('templates/footer');
     }
+
+    public function tambah()
+    {
+        $data['title'] = 'Tambah Metode Pembayaran';
+        $data['user'] = $this->pengguna->cekPengguna();
+        $data['metodePembayaran'] = $this->MetodePembayaran->getAllMetodePembayaran();
+        // $data['user'] = $this->db->get_where('user', [
+        //     'username' => $this->session->userdata('username')
+        // ])->row_array();
+
+        $this->form_validation->set_rules('nama_metodePembayaran', 'MetodePembayaran', 'required|trim|is_unique[metodePembayaran.nama_metodePembayaran]', [
+            'required'  => 'Metode Pembayaran harus diisi!',
+            'is_unique' => 'Metode Pembayaran sudah ada!'
+        ]);
+
+        if ($this->form_validation->run() == FALSE) {
+            $this->load->view('templates/header', $data);
+            $this->load->view('templates/topbar', $data);
+            $this->load->view('templates/sidebar', $data);
+            $this->load->view('master/metode-pembayaran/index');
+            $this->load->view('templates/footer');
+        } else {
+            $this->MetodePembayaran->tambah_metodePembayaran();
+        }
+    }
+
+    public function ubah()
+    {
+        $data['title'] = 'Ubah Metode Pembayaran';
+        $data['user'] = $this->pengguna->cekPengguna();
+        $data['metodePembayaran'] = $this->MetodePembayaran->getAllMetodePembayaran();
+        // $data['user'] = $this->db->get_where('user', [
+        //     'username' => $this->session->userdata('username')
+        // ])->row_array();
+
+        $this->form_validation->set_rules('nama_metodePembayaran', 'Metode Pembayaran', 'required|trim|is_unique[metodePembayaran.nama_metodePembayaran]', [
+            'required'   => 'Metode Pembayaran harus diisi!',
+            'is_unique' => 'Metode Pembayaran Sudah Ada'
+        ]);
+
+        if ($this->form_validation->run() == FALSE) {
+            $this->load->view('templates/header', $data);
+            $this->load->view('templates/topbar', $data);
+            $this->load->view('templates/sidebar', $data);
+            $this->load->view('master/metode-pembayaran/index');
+            $this->load->view('templates/footer');
+        } else {
+            $this->MetodePembayaran->ubah_metodePembayaran();
+        }
+    }
+
+    public function hapus($id_metodePembayaran)
+    {
+        $this->MetodePembayaran->hapus_metodePembayaran($id_metodePembayaran);
+    }
 }
--- application/models/MetodePembayaran_model.php
+++ application/models/MetodePembayaran_model.php
@@ -6,6 +6,48 @@ class MetodePembayaran_model extends CI_Model
 {
     public function getAllMetodePembayaran()
     {
-        return $this->db->get('metodePembayaran')->result_array();
+        return $this->db->get('metodepembayaran')->result_array();
+    }
+
+    public function tambah_metodePembayaran()
+    {
+        $data = [
+            'nama_metodePembayaran' => htmlspecialchars($this->input->post('nama_metodePembayaran', true)),
+        ];
+
+        $this->db->insert('metodePembayaran', $data);
+
+        $this->session->set_flashdata(
+            'message',
+            'ditambah.'
+        );
+        redirect('master/MetodePembayaran');
+    }
+
+    public function ubah_metodePembayaran()
+    {
+        $id_metodePembayaran = $this->input->post('id_metodePembayaran', true);
+        $data = [
+            'nama_metodePembayaran' => $this->input->post('nama_metodePembayaran', true),
+        ];
+
+        $this->db->where('id_metodePembayaran', $id_metodePembayaran);
+        $this->db->update('metodepembayaran', $data);
+
+        $this->session->set_flashdata(
+            'message',
+            'diubah.'
+        );
+        redirect('master/MetodePembayaran');
+    }
+
+    public function hapus_metodePembayaran($id_metodePembayaran)
+    {
+        $this->db->delete('metodepembayaran', ['id_metodePembayaran' => $id_metodePembayaran]);
+        $this->session->set_flashdata(
+            'message',
+            'dihapus.'
+        );
+        redirect('master/MetodePembayaran');
     }
 }
