5c3ac4f029b8102ef2b6ab8e0270972f92c89508
yhaae01 <suryaintpermana@gmail.com>
2022-05-26 22:38:51 +0700

    Menambahkan logo di title

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -6,6 +6,7 @@
   <meta charset="UTF-8" />
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link rel="icon" href="<?= base_url('assets') ?>/img/bhirama-logo.png" type="image/icon type">
   <link rel="stylesheet" href="<?= base_url('assets') ?>/css/bootstrap.min.css" />
   <link rel="stylesheet" href="<?= base_url('assets') ?>/vendor/fontawesome/css/all.min.css" />
 
