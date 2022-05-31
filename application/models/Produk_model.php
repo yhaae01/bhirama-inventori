4e3ca27cf85d09b7b917ea33ab5239a5d678a47d
ahmadmaulana10 <ahmadmaulana.am96@gmail.com>
2022-05-31 20:46:36 +0700

    hapus btn detail di Produk

--- application/models/Produk_model.php
+++ application/models/Produk_model.php
@@ -45,9 +45,6 @@ class Produk_model extends CI_Model
             $this->datatables->add_column(
                 'action',
                 '<div class="btn-group">' .
-                    form_open('master/Produk/detail/$1') .
-                    form_button(['type' => 'submit', 'title' => 'Tambah Detail', 'class' => 'btn btn-success', 'content' => '<i class="fas fa-tags"> </i>']) .
-                    form_close() . "&nbsp;" .
                     form_open('master/Produk/update/$1') .
                     form_button(['type' => 'submit', 'title' => 'Edit', 'class' => 'btn btn-warning', 'content' => '<i class="fas fa-pencil-alt"> </i>']) .
                     form_close() . "&nbsp;" .
