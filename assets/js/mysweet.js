b9c26878f709f02950d7f51cc0517684347ebe35
ahmadmaulana10 <ahmadmaulana.am96@gmail.com>
2022-06-04 16:30:14 +0700

    memperbaiki sweet alert

--- assets/js/mysweet.js
+++ assets/js/mysweet.js
@@ -1,17 +1,27 @@
 const flashData = $(".flash-data").data("flashdata");
 
 console.log(flashData);
-if (flashData == "Data tidak ditemukan.") {
+if (flashData == "tidak ditemukan.") {
 	Swal.fire({
 		title: "Gagal",
 		text: flashData,
+		icon: "error",
 		type: "danger",
+		showCloseButton: true,
+	});
+} else if (flashData == "Dihapus.") {
+	Swal.fire({
+		title: "Berhasil",
+		text: flashData,
+		icon: "success",
+		type: "success",
 		showCloseButton: true,
 	});
 } else if (flashData) {
 	Swal.fire({
 		title: "Berhasil",
 		text: "Data berhasil " + flashData,
+		icon: "success",
 		type: "success",
 		showCloseButton: true,
 	});
