dd82ec56b2a019ed963bbfe1675e0bdc82735487
yhaae01 <suryaintpermana@gmail.com>
2022-06-18 20:19:22 +0700

    Menghapus tombol ekspor excel dan word di pesanan

--- application/views/transaksi/pesanan/pesanan_list.php
+++ application/views/transaksi/pesanan/pesanan_list.php
@@ -12,10 +12,6 @@
                     <h4>Data Pesanan</h4>
                     <div class="card-header-action">
                         <?php echo anchor(site_url('transaksi/Pesanan/create'), '<i class="fas fa-plus"></i> Tambah Pesanan', 'class="btn btn-primary"'); ?>
-                        <?php if ($user['role'] != 'cs') : ?>
-                            <?php echo anchor(site_url('transaksi/Pesanan/word'), '<i class="fas fa-file-word"></i> Word', 'class="btn btn-info"'); ?>
-                            <?php echo anchor(site_url('transaksi/Pesanan/excel'), '<i class="fas fa-file-excel"></i> Excel', 'class="btn btn-success"'); ?>
-                        <?php endif; ?>
                     </div>
                 </div>
                 <div class="card-body">
